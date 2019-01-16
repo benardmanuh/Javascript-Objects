67fd7a1b9884785db29427a5d6c9fa8ddc70c94d
Benard Manuh <benardmanuh1@yahoo.co.uk>
2019-01-16 20:59:22 +0000

    Javascript Objects

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// This is a object that also contains a array called 'spells'.
+var user = {
+	name: "John",
+	age: 34,
+	hobby: "Football",
+	isMarried: false,
+	spells: ["Kamehameha","kayoken"],
+	shout: function() {
+		console.log("AAAh!");
+	}
+
+};
+// A array that contains two objects.
+var list = [
+
+	{
+		name: "Benard",
+		age: 23,
+
+	},
+
+	{
+		name: "Evelyn",
+		age: 26,
+
+	}
+
+];
+
+// Here below i have accessed 'Liverpool'.
+var plTeams = ["Liverpool","Manchester United"];
+
+console.log(plTeams[1]);
